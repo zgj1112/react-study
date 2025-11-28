b6e00407f9a5e66d5d6cca770aa60e30777fdd2a
zgj888 <2369394195@qq.com>
2025-11-28 18:00:59 +0800

    合并

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,19 @@
 /// <reference types="bmapgl" />
+// global.d.ts 或 vite-env.d.ts
+
+// 声明模块，告诉 TypeScript 导入 .png 文件时，它是一个字符串类型
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
+
+// 如果您还导入了其他静态资源，也可以一并声明
+declare module '*.svg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpg' {
+  const value: string;
+  export default value;
+}
